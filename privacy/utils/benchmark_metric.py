74a835942c5a6cf94798624f72dbf68bbd4b6b7d
Artheme <agauthier@student.ethz.ch>
2025-05-10 16:47:41 +0200

    default AUCmetric list

--- privacy/utils/benchmark_metric.py
+++ privacy/utils/benchmark_metric.py
@@ -23,7 +23,7 @@ class BenchmarkMetric(ABC):
 #Example of a BenchmarkMetric implementation using Area Under the Curve (AUC)
 class AUCMetric(BenchmarkMetric):
 
-    def __init__(self, rank_range: list[float]):
+    def __init__(self, rank_range: list[float] = [0, 0.17, 0.33, 0.5, 0.67, 0.83]):
         self.rank_range = rank_range
 
     def compute_metric(self, complexity: list[int], data_mean: list[float], data_std: list[float], baseline_score: float):
